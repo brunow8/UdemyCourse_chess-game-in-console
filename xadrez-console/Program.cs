8ec08e22b5195afdab0710ca8fb302fc3fee724f
Bruno Barbosa <brunobm19992014@gmail.com>
2021-09-02 13:55:47 +0100

    Criada a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
-
+using xadrez_console.Tabuleiro;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine($"Posição: {P}");
+
+
         }
     }
 }
